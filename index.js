2a74ddcaada025bac92d4619d283702c9ad24c3a
Sid Bentifraouine <sidbentifraouine@gmail.com>
2018-12-18 03:00:08 +0100

    feat(index):added support for jade and handlebars template engines

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var epxress = require('express')
-var app = epxress()
+const epxress = require('express')
+const engines = require('consolidate')
+const app = epxress()
 
-var fs = require('fs')
-var _ = require('lodash')
-var users = []
+const fs = require('fs')
+const _ = require('lodash')
+const users = []
 
 fs.readFile('users.json', { encoding: 'utf8' }, (err, data) => {
   if (err) throw err
@@ -14,17 +15,18 @@ fs.readFile('users.json', { encoding: 'utf8' }, (err, data) => {
   })
 })
 
-app.get('/', (req, res) => {
-  var buffer = ''
+app.engine('hbs', engines.handlebars)
 
-  users.forEach((user) => {
-    buffer += `<a href="/${user.username}">${user.name.full}</a><br>`
-  })
-  res.send(buffer)
+app.set('vews', './views')
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  // I can also say Index.jade in order to force rendering the jade one
+  res.render('index', { users })
 })
 
-// Route with regexp
 // NOTE: the order of Routes is important
+// Route with regexp
 app.get(/big.*/, (req, res, next) => {
   console.log('BIG USER ACCESS')
   next()
@@ -37,7 +39,7 @@ app.get(/.*dog.*/, (req, res, next) => {
 
 // Route with param
 app.get('/:username', (req, res) => {
-  var username = req.params.username
+  const username = req.params.username
   res.send(username)
 })
 
@@ -45,6 +47,6 @@ app.get('/bonsoir', (req, res) => {
   res.send('Bonsoir!')
 })
 
-var server = app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('Server running at http://localhost:' + server.address().port)
 })
